61f707dd2e3f2dcfeba9c08769058d1f7adfd211
jianhuinie <niejianhui@baijiahulian.com>
2018-07-27 13:45:48 +0800

    change

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
+const htmlPlugin = require('html-webpack-plugin');
+const uglify = require('uglifyjs-webpack-plugin');
+//分离样式
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const extractTextWebpackPlugin = require('extract-text-webpack-plugin'); //不推荐使用了
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -6,26 +16,82 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: "http://localhost:8888/"
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
+                // use: [
+                //     {loader: "style-loader"},
+                //     {loader: "css-loader"}
+                // ]
+                loaders: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                loaders: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)/,  
                 use: [
-                    {loader: "style-loader"},
-                    {loader: "css-loader"}
+                    {
+                        loader:'url-loader', 
+                        options: {
+                            limit: 8192,
+                            outputPath:'images/'  
+                        }
+                    }
                 ]
+            },
+            {
+                test: /\.(woff|svg|woff2|eot|ttf|otf)$/,
+                loader: 'url-loader'
+            },
+            {
+                test:/\.(jsx|js)$/,
+                use:{
+                    loader:'babel-loader'
+                    // options:{
+                    //     presets:[
+                    //         "es2015","react"
+                    //     ]
+                    // }
+                },
+                exclude:/node_modules/
             }
         ]
     },
     plugins: [
-        
+        new htmlPlugin({
+            minify: { 
+                removeAttributeQuotes: true  
+            },
+            hash: true, 
+            template: './src/index.html'
+           
+        }),
+        new uglify(),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[name].[id].css'
+        }),
+        new CleanWebpackPlugin([path.resolve(__dirname, '../dist')], {
+            root: path.resolve(__dirname, '../') 
+        }),
     ],
     devServer: {
         contentBase: path.resolve(__dirname, '../dist'),
         host: 'localhost',
         compress: true,
+        hot: true,
         port: 8888
-    }
+    },
+    optimization: {
+        minimizer: [
+          new OptimizeCssAssetsPlugin({})
+        ]
+    },
 };
--- src/main.js
+++ src/main.js
@@ -11,5 +11,8 @@
 //     />
 // ), document.getElementById('app'));
 import indexCss from './css/index.css';
+import indexLess from './index.less';
+const sayhello = 'hello webpack';
+alert(sayhello);
 
 document.getElementById("app").innerHTML="hello webpack";
